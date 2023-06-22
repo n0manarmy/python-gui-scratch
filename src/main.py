bbd1dacacace1320bdb517d27d7fd4da5bb68e0e
Penn Wilbert <pwilbert@gmail.com>
2023-06-22 10:35:42 -0400

    framing with grid, sizing interal with pack

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ import re
 class AppGUI(tk.Frame):
 
     JIRA_TICKET_PATTERN = "([A-z]{3}-[0-9]{1,})"
-    PAD_SIZE = 10
+    PAD_SIZE = 5
     dest = None
 
     def __init__(self):
@@ -22,57 +22,52 @@ class AppGUI(tk.Frame):
         self.init_copy_ui()
         self.build_text_area()
         self.pack(fill=tk.BOTH)
-        self.pack_propagate(0)
-
-        # self.pack(fill=tk.BOTH)
-        # self.grid(columnspan=10)
 
 
     def build_text_area(self):
-        # this_frame = tk.Frame(master=self, width=400, height=200)
-        v_scroll_bar = tk.Scrollbar(self)
-        v_scroll_bar.grid(row=1, rowspan=10, column=11, sticky=tk.E+tk.N+tk.S, padx=self.PAD_SIZE, pady=self.PAD_SIZE, ipadx=2)
-        # v_scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
-        
-        h_scroll_bar = tk.Scrollbar(self, orient=tk.HORIZONTAL)
-        h_scroll_bar.grid(row=11, column=1, columnspan=10, sticky=tk.E+tk.W, padx=self.PAD_SIZE, pady=self.PAD_SIZE, ipady=2)
-        # h_scroll_bar.pack(side=tk.BOTTOM, fill=tk.X)
+        scroll_frame = tk.Frame(master=self, bg="red")
 
-        self.list_box = tk.Listbox(self, xscrollcommand=h_scroll_bar.set, yscrollcommand=v_scroll_bar.set, width=100, height=25)
-        self.list_box.grid(row=1, rowspan=10, column=1, columnspan=10, padx=self.PAD_SIZE, pady=self.PAD_SIZE)
-        # self.list_box.pack(side=tk.BOTTOM, fill=tk.BOTH)
+        v_scroll_bar = tk.Scrollbar(scroll_frame, orient=tk.VERTICAL)
+        # v_scroll_bar.grid(row=1, rowspan=10, column=11, sticky=tk.E+tk.N+tk.S, padx=self.PAD_SIZE, pady=self.PAD_SIZE, ipadx=2)
+        v_scroll_bar.pack(side=tk.RIGHT, fill=tk.Y, padx=self.PAD_SIZE, pady=self.PAD_SIZE, ipadx=5)
+        
+        h_scroll_bar = tk.Scrollbar(scroll_frame, orient=tk.HORIZONTAL)
+        # h_scroll_bar.grid(row=11, column=1, columnspan=10, sticky=tk.E+tk.W, padx=self.PAD_SIZE, pady=self.PAD_SIZE, ipady=2)
+        h_scroll_bar.pack(side=tk.BOTTOM, fill=tk.X, padx=self.PAD_SIZE, pady=self.PAD_SIZE, ipady=5)
+
+        self.list_box = tk.Listbox(scroll_frame, xscrollcommand=h_scroll_bar.set, yscrollcommand=v_scroll_bar.set, width=100, height=25)
+        # self.list_box.grid(row=1, rowspan=10, column=1, columnspan=10, padx=self.PAD_SIZE, pady=self.PAD_SIZE, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.list_box.pack(side=tk.BOTTOM, fill=tk.BOTH, padx=self.PAD_SIZE, pady=self.PAD_SIZE)
         
         v_scroll_bar.config(command=self.list_box.yview)
         h_scroll_bar.config(command=self.list_box.xview)
 
-        # this_frame.grid(row=1,column=1,rowspan=10,columnspan=10)
-        
-        # scroll_frame.grid(row=5, column=0, columnspan=4, padx=self.PAD_SIZE, pady=self.PAD_SIZE)
-        # self.pack(side=tk.BOTTOM, fill=tk.BOTH)
-        
+        scroll_frame.grid(row=1, column=1, padx=self.PAD_SIZE, pady=self.PAD_SIZE)
+
 
     def init_copy_ui(self):
-        # this_frame = tk.Frame(master=self)
-        self.select_src_button = tk.Button(self, text="src", command=lambda : self.load_files_into_list())
-        self.select_src_button.grid(row=1, column=0, padx=self.PAD_SIZE, pady=self.PAD_SIZE)
-        # self.select_src_button.pack(side=tk.TOP, fill=tk.BOTH)
+        button_frame = tk.Frame(master=self)
+        button_frame.config(bg="black")
 
-        self.select_dst_button = tk.Button(self, text="dst", command=lambda : self.select_dst_dir())
-        self.select_dst_button.grid(row=2, column=0, padx=self.PAD_SIZE, pady=self.PAD_SIZE)
+        self.select_src_button = tk.Button(button_frame, text="src", command=lambda : self.load_files_into_list())
+        self.select_src_button.pack(side=tk.TOP, fill=tk.BOTH, padx=self.PAD_SIZE, pady=self.PAD_SIZE)
+        # self.select_src_button.grid(row=1, column=0, padx=self.PAD_SIZE, pady=self.PAD_SIZE, sticky=tk.N)
 
-        # self.select_dst_button.pack(side=tk.TOP, fill=tk.BOTH)
+        self.select_dst_button = tk.Button(button_frame, text="dst", command=lambda : self.select_dst_dir())
+        self.select_dst_button.pack(side=tk.TOP, fill=tk.BOTH, padx=self.PAD_SIZE, pady=self.PAD_SIZE)
+        # self.select_dst_button.grid(row=2, column=0, padx=self.PAD_SIZE, pady=self.PAD_SIZE, sticky=tk.N)
 
-        self.init_copy_button = tk.Button(self, text="copy", command=lambda : self.thread_copy_files(), state=tk.DISABLED)
-        self.init_copy_button.grid(row=3, column=0, padx=self.PAD_SIZE, pady=self.PAD_SIZE)
-        # self.init_copy_button.pack(side=tk.TOP, fill=tk.BOTH)
+        self.init_copy_button = tk.Button(button_frame, text="copy", command=lambda : self.thread_copy_files(), state=tk.DISABLED)
+        self.init_copy_button.pack(side=tk.TOP, fill=tk.BOTH, padx=self.PAD_SIZE, pady=self.PAD_SIZE)
+        # self.init_copy_button.grid(row=3, column=0, padx=self.PAD_SIZE, pady=self.PAD_SIZE, sticky=tk.N)
 
-        # self.pack(side=tk.LEFT, padx=10, pady=10)
-        # select_source_button.grid(row=0, column=0)
+        self.exit_button = tk.Button(button_frame, text="Exit", command=lambda : exit())
+        self.exit_button.pack(side=tk.BOTTOM, fill=tk.BOTH, padx=self.PAD_SIZE, pady=self.PAD_SIZE)
+        # self.exit_button.grid(row=10, column=0, padx=self.PAD_SIZE, pady=self.PAD_SIZE, sticky=tk.S)
 
+        button_frame.grid(row=1, column=0, rowspan=10, sticky=tk.N+tk.S, padx=self.PAD_SIZE, pady=self.PAD_SIZE)    
 
     def thread_copy_files(self):
-        # t1=Thread(target=self.copy_files)
-        # t1.start()
         self.queue = queue.Queue()
         self.select_src_button.config(state=tk.DISABLED)
         self.select_dst_button.config(state=tk.DISABLED)
@@ -128,41 +123,9 @@ class AppGUI(tk.Frame):
         self.user_sid_entry = tk.Entry(self)
         self.user_sid_entry.grid(row=2, column=1, padx=1, pady=1)
 
-        # self.ticket_number_label.pack()
         self.print_values_button = tk.Button(self, text="Print results", command=self.print_results, state=tk.DISABLED)
-        # self.print_values_button.bind("<<valid_ticket>>", self.activate_print_results)
         self.print_values_button.grid(row=4,column=2,padx=10,pady=10)
 
-        self.exit_button = tk.Button(self,text="Exit", command=self.exit_app)
-        self.exit_button.grid(row=3, column=2, padx=10, pady=10)
-        # self.exit_button.pack()
-    
-    
-    def validate_jira_ticket(self, input):
-        print("validate_jira_ticket, input: ", input)
-        if re.match(self.JIRA_TICKET_PATTERN, input):
-            print("validate_jira_ticket TRUE")
-            self.print_values_button.config(state=tk.ACTIVE)
-            return True
-        else:
-            print("validate_jira_ticket FALSE")
-            self.print_values_button.config(state=tk.DISABLED)
-            return False
-    
-
-    def exit_app(self):
-        sys.exit()
-
-
-    def print_results(self):
-        self.validate_jira_ticket(self.ticket_number_entry.get())
-        # print()
-        print("ticket: ", self.ticket_number_entry.get())
-        print("sid: ", self.user_sid_entry.get())
-
-
-    def activate_print_results(self, event):
-        event.widget["state"] = tk.ACTIVE
 
 
 my_app = AppGUI()
